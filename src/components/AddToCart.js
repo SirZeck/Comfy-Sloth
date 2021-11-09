38c9495de88c1082a65f7284176021adae4950fa
Aditya Raj <adithebest.ar@gmail.com>
2021-11-09 12:32:26 +0530

    colors button set
    Signed-off-by: Aditya Raj <adithebest.ar@gmail.com>

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -5,8 +5,35 @@ import { FaCheck } from 'react-icons/fa'
 import { useCartContext } from '../context/cart_context'
 import AmountButtons from './AmountButtons'
 
-const AddToCart = () => {
-  return <h4>addToCart </h4>
+const AddToCart = ({ product }) => {
+  const { id, stock, colors } = product
+
+  const [mainColor, setMainColor] = useState(colors[0])
+
+  return (
+    <Wrapper>
+      <div className='colors'>
+        <span>Colors : </span>
+        <div>
+          {colors.map((color, index) => {
+            return (
+              <button
+                style={{ background: color }}
+                className={`${
+                  mainColor === color ? 'color-btn active' : 'color-btn'
+                }`}
+                key={index}
+                onClick={() => setMainColor(color)}
+              >
+                {mainColor === color ? <FaCheck /> : null}
+              </button>
+            )
+          })}
+        </div>
+      </div>
+      <div className='btn-container'></div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
