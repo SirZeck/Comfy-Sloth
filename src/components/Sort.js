fe3df3815a8c3b3396185331d152b5c0af70477c
Aditya Raj <adithebest.ar@gmail.com>
2021-11-09 18:04:51 +0530

    sort func init
    Signed-off-by: Aditya Raj <adithebest.ar@gmail.com>

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -8,6 +8,8 @@ const Sort = () => {
     grid_view,
     setGridView,
     setListView,
+    sort,
+    updateSort,
   } = useFilterContext()
   return (
     <Wrapper>
@@ -31,11 +33,17 @@ const Sort = () => {
       <hr />
       <form>
         <label htmlFor='sort'>sort by</label>
-        <select name='sort' id='sort' className='sort-input'>
+        <select
+          name='sort'
+          id='sort'
+          className='sort-input'
+          value={sort}
+          onChange={updateSort}
+        >
           <option value='price-lowest'>price (lowest)</option>
           <option value='price-highest'>price (highest)</option>
           <option value='name-a'>name (a-z)</option>
-          <option value='price-lowest'>name (z-a)</option>
+          <option value='name-z'>name (z-a)</option>
         </select>
       </form>
     </Wrapper>
