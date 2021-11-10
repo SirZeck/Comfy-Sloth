0a7d631b34c2d45070698471ee676552c8da247c
Aditya Raj <adithebest.ar@gmail.com>
2021-11-10 15:49:35 +0530

    login/logout func added
    Signed-off-by: Aditya Raj <adithebest.ar@gmail.com>

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -19,10 +19,14 @@ const CartButtons = () => {
           <span className='cart-value'>{total_items}</span>
         </span>
       </Link>
-      <button type='button' className='auth-btn'>
+      <button type='button' className='auth-btn' onClick={loginWithRedirect}>
         Login <FaUserPlus />
       </button>
-      <button type='button' className='auth-btn'>
+      <button
+        type='button'
+        className='auth-btn'
+        onClick={() => logout({ returnTo: window.location.origin })}
+      >
         Logout <FaUserMinus />
       </button>
     </Wrapper>
